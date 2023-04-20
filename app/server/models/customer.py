5ff23407f566fbd1b2f9efb679423ea0033b3a4a
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-20 11:08:37 -0600

    added custom model with example

--- app/server/models/customer.py
+++ app/server/models/customer.py
@@ -1,3 +1,32 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr,Field
+from pydantic import BaseModel, Field
+
+
+
+class CustomerSchema(BaseModel):
+    firstName: str = Field(...)
+    lastName: str = Field(...)
+    streetAddress: str = Field(...)
+    streetAddress2: str = Field(...) | None
+    city: str = Field(...)
+    # if country code is not USA, state and zipcode can be used for country-specific values that are equivalents
+    state: str = Field(...) | None
+    zipcode: int = Field(...) | None
+    # If not provided, country code defaults to USA country code (ISO 3166 standard) (USA: US)
+    country: str = Field(...) | None
+
+    
+    class Config: 
+        schema_extra = {
+            "example": {
+            "firstName" : "Mary",
+            "lastName" : "Douglass",
+            "streetAddress": "15566 Michigan Ave",
+            "streetAddress2": "Unit C",
+            "city": "Columbus",
+            "state": "Ohio",
+            "zipcode": 43004,
+            "country": "US"
+            }
+        }
