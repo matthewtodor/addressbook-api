88e8a1681099439acfb1e6888fd96d3906a3eb40
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-20 13:17:23 -0600

    added route to get all customers from the database

--- app/server/database.py
+++ app/server/database.py
@@ -1,5 +1,7 @@
 import motor.motor_asyncio
 
+from bson.objectid import ObjectId
+
 MONGO_DETAILS = "mongodb://localhost:27017"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
@@ -24,5 +26,11 @@ def customer_helper(customer) -> dict:
         "state": customer["state"],
         "zipcode": customer["zipcode"],
         "country": customer["country"]
-
     }
+
+#! get all customers
+async def retrieve_customers():
+    customers = []
+    async for customer in customer_collection.find():
+        customers.append(customer_helper(customer))
+    return customers
