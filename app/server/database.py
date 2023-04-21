7a188d9c89e0c42c27216c7920910e480715500a
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-20 21:26:37 -0600

    added the remainder of CRUD for the customer data

--- app/server/database.py
+++ app/server/database.py
@@ -28,9 +28,42 @@ def customer_helper(customer) -> dict:
         "country": customer["country"]
     }
 
-#! get all customers
+# get all customers
 async def retrieve_customers():
     customers = []
     async for customer in customer_collection.find():
         customers.append(customer_helper(customer))
     return customers
+
+# get customer by ID
+async def retrieve_customer_by_id(id:str) -> dict:
+    customer = await customer_collection.find_one({"_id" : ObjectId(id)})
+    if customer:
+        return customer_helper(customer)
+    
+# Add new customer
+async def add_customer(customer_data:dict) -> dict:
+    customer = await customer_collection.insert_one(customer_data)
+    new_customer = await customer_collection.find_one({"_id" : customer.inserted_id})
+    return customer_helper(new_customer)
+
+
+# Update customer
+async def update_customer(id:str, data:dict):
+    if len(data) < 1:
+        return False
+    customer = await customer_collection.find_one({"_id" : ObjectId(id)})
+    if customer:
+        updated_customer = await customer_collection.update_one(
+            {"_id" : ObjectId(id)}, {"$set" : data}
+        )
+        if updated_customer:
+            return True
+        return False
+    
+# Delete customer
+async def delete_customer(id:str):
+    customer = await customer_collection.find_one({"_id" : ObjectId(id)})
+    if customer:
+        await customer_collection.delete_one({"_id" : ObjectId(id)})
+        return True
